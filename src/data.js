3d329827e41f32d7cdd5cdbd09ff75860cb9d4d1
Your Name <you@example.com>
2020-02-25 11:47:20 +0530

    we changed data

--- src/data.js
+++ src/data.js
@@ -6,24 +6,24 @@ https://www.iconfinder.com/Makoto_msk */
 export const storeProducts = [
   {
     id: 1,
-    title: "Google Pixel - Black",
+    title: "Denill Latest Collection",
     img: "img/product-1.png",
-    price: 10,
-    company: "GOOGLE",
+    price: 398,
+    company: "rubber",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Sole : rubber Shoe Width: Medium The fourth generation of our legendary Speed cross lug pattern is an even hungrier monster for eating up soft, technical trails.Active & Smart; Extremely Durable; Provides endless comfort & sturdiness. Lifestyle: Casual wear, Office wear, Daily wear, College wear, Party wear, Summer wearExtremely lightweight material, these are nice Shoes for everyday use, or wedding .this unique design and stylish Shoes are to maximize your fashion.Care Instruction - Do not wash. Use good quality brush or clean cloth to remove dirt.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 2,
-    title: "Samsung S7",
+    title: "Salwar- Suit Dress Material",
     img: "img/product-2.png",
-    price: 16,
-    company: "SAMSUNG",
+    price: 999,
+    company: "Cotton Silk",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Unstitched Denim Cotton Dress Material For Women Top: Denim Cotton (2.5 meter) Work Type: Embroidered Bottom: Plain Cotton Bottom ( Aprrox 2.25 meter) Dupatta: Chiffon Dupatta Brush Printed (Aprrox 2.25 meter) Ideal for Festivals, Parties, Special occasion, Official or casual wear and Social get together",
     inCart: false,
     count: 0,
     total: 0
@@ -42,61 +42,61 @@ export const storeProducts = [
   },
   {
     id: 4,
-    title: "HTC 10 - White",
+    title: "Bluetooth Headset",
     img: "img/product-4.png",
-    price: 18,
-    company: "htc",
+    price: 1329,
+    company: "Jabra",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Incorporates audio technology that has been optimized to make conversations clear.Features an intuitive design that makes pairing simple and easy to operate.Stream music, podcasts and GPS directions.With up to 6 hours of battery on a single charge.Connect 2 devices at the same time",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 5,
-    title: "HTC Desire 626s",
+    title: "Watches Combo Look",
     img: "img/product-5.png",
-    price: 24,
-    company: "htc",
+    price: 348,
+    company: "BK",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "Warranty type:Manufacturer; 1 Years manufacturer warranty Watch Movement Type: Quartz; Watch Display Type: Analog Band Color:Orange,Sky blue,Black ; Band Material: Leather Dial Color:Orange,Sky blue,Black ; Case Shape: Round; Dial Glass Material: Brass Water Resistance Depth: 30 meters; Buckle Clasp",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 6,
-    title: "Vintage Iphone",
+    title: "Silver Plated Charm Bracelet",
     img: "img/product-6.png",
-    price: 17,
-    company: "apple",
+    price: 199,
+    company: "Silver",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      "BEAUTY-A Must have piece to add to your Jewellery Treasure QUALITY-5 Layer Micro Plated. Thickness of plating: 0.1 Micron, where .03 Micron Plating available in Market.SKIN FRIENDLY- Alloy (97% tin and 3% bismuth). Nickel free and Lead free as per International Standards that makes it very skin friendly.Size- Adjustable.GIFT FOR HER! - Ideal Valentine, Birthday, Anniversary gift for someone you ❤ LOVE ❤. With our packaging Box, you do not need to opt-in for any additional gift packaging. The product comes in a beautiful Elegant Ready-to-Gift Box.",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 7,
-    title: "Iphone 7",
+    title: "Pepe Jeans Girl's Jacket",
     img: "img/product-7.png",
-    price: 30,
-    company: "apple",
+    price: 1117,
+    company: "Cotton",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      " Care Instructions: machine wash 100% Cotton machine wash",
     inCart: false,
     count: 0,
     total: 0
   },
   {
     id: 8,
-    title: "Smashed Iphone",
+    title: "talian Designer Gold Plated",
     img: "img/product-8.png",
-    price: 2,
-    company: "apple",
+    price: 275,
+    company: "Gold",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
-    inCart: false,
+      "Colour : Rose Gold Material: Gold Plated It is advisable to store jewellery in a zip lock pouch (air tight pouch), keep away from water perfume and other chemicals and clean it with dry and soft cloth stylish one piece rose gold plated anklet. adjustable length suitable for most feet.a mere look will be enough for you to fall in love with these pretty anklets from shining diva.ideal valentine, birthday, anniversary gift for someone you love stylish one piece rose gold plated anklet. adjustable length suitable for most feet.",
+    inCart: false,  
     count: 0,
     total: 0
   }
